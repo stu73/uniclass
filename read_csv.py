8e8f26c44467fd6cfc2410fa29f69cf1fa413466
stu73 <stuart.chalmers@gmail.com>
2013-12-14 17:25:52 +0000

    Fixed python formatting - need to add full xml

--- read_csv.py
+++ read_csv.py
@@ -1,50 +1,59 @@
 import csv
 import sys
-import xml.etree.cElementTree as ET
-import xml.dom.minidom
 
 uniclass2_array = []
 
 cmdargs = str(sys.argv)
 
 with open(str(sys.argv[1]), 'rb') as csvfile:
-  spamreader = csv.reader(csvfile, delimiter=',', quotechar='\"')
+  spam = csv.reader(csvfile, delimiter=',', quotechar='\"')
   count = 0
-  for row in spamreader:
+  for row in spam:
 	uniclass2_array.append([]) 
 	uniclass2_array[count] = row
 	count += 1
 
+# rest counter
 count = 0
 
-# For each line in array
-
-# count size of code
-# if code  
-
-
+print len(uniclass2_array)
 
 for row in uniclass2_array:
-	# if it's an 11 - it's the last in the list, do a normal tag
+
 	if len(uniclass2_array[count][0]) == 11:
-		print "\t<DataEntry number=\""+uniclass2_array[count][0]+"\" title=\""+uniclass2_array[count][1]+"\"/>"
-		# if the next in line is an 8, up one in tree - close the tag		
-		if len(uniclass2_array[count+1][0]) == 8:
-			print "</DataEntry>"
-		if len(uniclass2_array[count+1][0]) == 5:
-			print "</DataEntry>"
-	# if it's an 8 - it may have children - check if next one is 11 - if so, dont close tag	
+		print "3-------<d>"+uniclass2_array[count][0]+"</d>"
+		if (count < len(uniclass2_array)-1):		
+			if(len(uniclass2_array[count+1][0]) == 8):
+				print "2----</d>"
+
 	elif len(uniclass2_array[count][0]) == 8:
-		if len(uniclass2_array[count+1][0]) == 11:
-			print "<DataEntry number=\""+uniclass2_array[count][0]+"\" title=\""+uniclass2_array[count][1]+"\">"
-		elif len(uniclass2_array[count+1][0]) == 8:
-			print "<DataEntry number=\""+uniclass2_array[count][0]+"\" title=\""+uniclass2_array[count][1]+"\"/>"
-	# if it's a 5 - it may have children - check if next one is 8 - if so, dont close tag	
+		print "2----<d>"+uniclass2_array[count][0]
+		if (count < len(uniclass2_array)-1):		
+			if(len(uniclass2_array[count+1][0]) == 8):
+				print "2----</d>"
+			if(len(uniclass2_array[count+1][0]) == 5):
+				print "2----</d>"
+
 	elif len(uniclass2_array[count][0]) == 5:
-		if len(uniclass2_array[count+1][0]) == 8:
-			print "\t<DataEntry number=\""+uniclass2_array[count][0]+"\" title=\""+uniclass2_array[count][1]+"\">"
-		elif len(uniclass2_array[count+1][0]) == 5:
-			print "<DataEntry number=\""+uniclass2_array[count][0]+"\" title=\""+uniclass2_array[count][1]+"\"/>"
+		if count > 0:
+			if(len(uniclass2_array[count-1][0]) == 11):
+				print "2----</d>"
+				print "1-</d>"
+			if(len(uniclass2_array[count-1][0]) == 8):
+				print "2----</d>"			
+		print "1-<d>"+uniclass2_array[count][0]
+
 	count +=1
 
+if(len(uniclass2_array[len(uniclass2_array)-1][0]) == 11):
+	print "3-------</d>"
+	print "2----</d>"
+	print "1-</d>"
+
+if(len(uniclass2_array[len(uniclass2_array)-1][0]) == 8):
+	print "2----</d>"
+	print "1-</d>"
+
+if(len(uniclass2_array[len(uniclass2_array)-1][0]) == 5):
+	print "1-</d>"
 
